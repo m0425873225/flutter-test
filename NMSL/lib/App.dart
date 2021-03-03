06f34b84982137f5b3ba7aff65470df1a545ba58
m0425873225 <79737996+m0425873225@users.noreply.github.com>
2021-03-03 22:45:23 +0800

    commit -3

---
+++ NMSL/lib/App.dart
@@ -0,0 +1,152 @@
+import 'package:NMSL/search.dart';
+import 'package:flutter/material.dart';
+import 'package:NMSL/home.dart';
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext) {
+    return MaterialApp(
+      theme: new ThemeData(brightness: Brightness.dark),
+      home: AppPage(),
+    );
+  }
+}
+
+class AppPage extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<AppPage> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [Home(), Search()];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '首頁'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: '搜尋'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: '設定'),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ClipOval(
+                        child: Image.network(
+                          'https://www.steamxo.com/wp-content/uploads/2019/11/5ggL5q154529_874683.jpg',
+                          fit: BoxFit.cover,
+                          width: 70,
+                          height: 70,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'NMSL',
+                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      ),
+                      Icon(
+                        Icons.add_circle_outline,
+                        size: 30,
+                        color: Colors.blue,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'yan@gmail.com',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '@fuckUbitch',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 12,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person_outline),
+              title: Text(
+                '個人資訊',
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text(
+                '列表',
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.ac_unit),
+              title: Text(
+                'test1',
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text(
+                'test2',
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ NMSL/lib/search.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+
+class Search extends StatefulWidget {
+  @override
+  _SearchPage createState() =>_SearchPage();
+}
+
+
+class Product {
+  final String tittle;
+  final String tag;
+  final String num;
+
+  Product({this.tittle, this.tag, this.num});
+}
+
+class _SearchPage extends State<Search>{
+  final List<Product> listItems = new List<Product>.generate(500, (i) {
+    return Product(
+      tittle: 'title: $i',
+      tag: '#tag：$i',
+      num: 'num: $i',
+    );
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+    print('width is $width; height is $height');
+
+    return SingleChildScrollView(
+        child: Center(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Icon(Icons.accessibility),
+                    ),
+                    Container(
+                      width: 250,
+                      child: TextField(
+                        autofocus: false,
+                        decoration: InputDecoration(
+                            fillColor: Colors.grey,
+                            filled: true,
+                            contentPadding: EdgeInsets.all(10.0),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                                borderSide: BorderSide(
+                                    color: Colors.black,
+                                    width: 3.0,
+                                    style: BorderStyle.solid)),
+                            hintText: "搜尋",
+                            prefixIcon: Icon(Icons.search, color: Colors.black)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Icon(Icons.settings),
+                    ),
+                  ],
+                ),
+                Container(
+                  /*padding:EdgeInsets.symmetric(vertical: 20.0, horizontal: 40),*/
+                  child: _TESTLIST(listItems),
+                ),
+              ],
+            )));
+  }
+  Widget _TESTLIST(listdata) {
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+    return Container(
+      height: 500,
+      child: ListView.builder(
+          itemCount: listItems.length,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20),
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${listItems[index].tittle}',
+                    style: TextStyle(color: Colors.grey, fontSize: 12),
+                  ),
+                  Text(
+                    '${listItems[index].tag}',
+                    style: TextStyle(color: Colors.white, fontSize: 22),
+                  ),
+                  Text(
+                    '${listItems[index].num}',
+                    style: TextStyle(color: Colors.grey, fontSize: 12),
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                //                    <-- BoxDecoration
+                border: Border(bottom: BorderSide(color: Colors.grey)),
+              ),
+            );
+          }),
+    );
+  }
+}
