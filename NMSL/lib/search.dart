7d3e1c294814c21efc666b052140f63d9f4c52aa
m0425873225 <79737996+m0425873225@users.noreply.github.com>
2021-03-17 23:30:52 +0800

    commit-0317-4

--- NMSL/lib/search.dart
+++ NMSL/lib/search.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class SearchBar extends StatefulWidget{
   @override
@@ -36,76 +38,144 @@ class Search extends StatefulWidget {
   _SearchPage createState() =>_SearchPage();
 }
 
-
-class Product {
-  final String tittle;
-  final String tag;
-  final String num;
-
-  Product({this.tittle, this.tag, this.num});
-}
-
-class _SearchPage extends State<Search>{
-  final List<Product> listItems = new List<Product>.generate(500, (i) {
-    return Product(
-      tittle: 'title: $i',
-      tag: '#tag：$i',
-      num: 'num: $i',
-    );
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final width = size.width;
-    final height = size.height;
-    print('width is $width; height is $height');
-
-    return SingleChildScrollView(
-        child: Center(
-            child: Column(
-              children: [
-                Container(
-                  child: _TESTLIST(listItems),
-                ),
-              ],
-            )));
+class _SearchPage extends State<Search> {
+  List<blockchainApi> blockchaindata ;
+  Future<List<blockchainApi>> _futureBlockchain;
+  Future<List<blockchainApi>> BlockchainApi() async{
+    try {
+      Uri _uri = Uri.parse('https://api.coinlore.net/api/tickers/');
+      final data =  await http.get(_uri);
+      if(data.statusCode == 200){
+        if(json.decode(data.body)['success']){
+          List dk = json.decode(data.body)['data'] as List;
+          return dk.map((e) => blockchainApi.fromJson(e)).toList();
+        }
+      } else if (data.statusCode == 404){return null;}
+      else{return null;}
+    }
+    catch (e) {
+      throw Exception(e);
+    }
   }
-  Widget _TESTLIST(listdata) {
-    final size = MediaQuery.of(context).size;
-    final width = size.width;
-    final height = size.height;
-    return Container(
-      height: 600,
-      child: ListView.builder(
-          itemCount: listItems.length,
-          itemBuilder: (context, index) {
-            return Container(
-              padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20),
-              child: new Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '${listItems[index].tittle}',
-                    style: TextStyle(color: Colors.grey, fontSize: 12),
-                  ),
-                  Text(
-                    '${listItems[index].tag}',
-                    style: TextStyle(color: Colors.white, fontSize: 22),
-                  ),
-                  Text(
-                    '${listItems[index].num}',
-                    style: TextStyle(color: Colors.grey, fontSize: 12),
-                  ),
-                ],
-              ),
-              decoration: BoxDecoration(
-                //                    <-- BoxDecoration
-                border: Border(bottom: BorderSide(color: Colors.grey)),
+
+  FutureBuilder _futureHmapi(){
+    return FutureBuilder(
+        future: this._futureBlockchain,
+        builder: (context,projectSnap){
+          if ((projectSnap.connectionState == ConnectionState.none)||
+              (projectSnap.hasData == null)||
+              (projectSnap.data == null))
+          {return Container();}
+          else{
+            this.blockchaindata= projectSnap.data;
+            return Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: this.blockchaindata.length,
+                itemBuilder: (context,index){
+                  return ListTile(
+                    title: Text('${blockchaindata[index].name}'),
+                    subtitle: Text('${blockchaindata[index].priceUsd}'),
+                  );
+                },
               ),
             );
-          }),
+          }
+        });
+  }
+  @override
+  void initState(){
+    this._futureBlockchain = this.BlockchainApi();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            child: _futureHmapi(),
+          )
+        ],
+      ),
     );
+  }
+}
+
+
+class blockchainApi {
+  String id;
+  String symbol;
+  String name;
+  String nameid;
+  int rank;
+  String priceUsd;
+  String percentChange24h;
+  String percentChange1h;
+  String percentChange7d;
+  String priceBtc;
+  String marketCapUsd;
+  double volume24;
+  double volume24a;
+  String csupply;
+  String tsupply;
+  String msupply;
+
+  blockchainApi(
+      {this.id,
+        this.symbol,
+        this.name,
+        this.nameid,
+        this.rank,
+        this.priceUsd,
+        this.percentChange24h,
+        this.percentChange1h,
+        this.percentChange7d,
+        this.priceBtc,
+        this.marketCapUsd,
+        this.volume24,
+        this.volume24a,
+        this.csupply,
+        this.tsupply,
+        this.msupply});
+
+  blockchainApi.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    symbol = json['symbol'];
+    name = json['name'];
+    nameid = json['nameid'];
+    rank = json['rank'];
+    priceUsd = json['price_usd'];
+    percentChange24h = json['percent_change_24h'];
+    percentChange1h = json['percent_change_1h'];
+    percentChange7d = json['percent_change_7d'];
+    priceBtc = json['price_btc'];
+    marketCapUsd = json['market_cap_usd'];
+    volume24 = json['volume24'];
+    volume24a = json['volume24a'];
+    csupply = json['csupply'];
+    tsupply = json['tsupply'];
+    msupply = json['msupply'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['symbol'] = this.symbol;
+    data['name'] = this.name;
+    data['nameid'] = this.nameid;
+    data['rank'] = this.rank;
+    data['price_usd'] = this.priceUsd;
+    data['percent_change_24h'] = this.percentChange24h;
+    data['percent_change_1h'] = this.percentChange1h;
+    data['percent_change_7d'] = this.percentChange7d;
+    data['price_btc'] = this.priceBtc;
+    data['market_cap_usd'] = this.marketCapUsd;
+    data['volume24'] = this.volume24;
+    data['volume24a'] = this.volume24a;
+    data['csupply'] = this.csupply;
+    data['tsupply'] = this.tsupply;
+    data['msupply'] = this.msupply;
+    return data;
   }
 }
